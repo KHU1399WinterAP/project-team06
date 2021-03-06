59506be1844335dcf8391daead58f670ff843e64
A.Reghabi <a.reghabi1380@gmail.com>
2021-03-06 12:43:04 +0330

    Adding MainMenu created by JFormDesigner

---
+++ QuizOfKings/src/main/java/gui/MainMenu.java
@@ -0,0 +1,39 @@
+/*
+ * Created by JFormDesigner on Sat Mar 06 12:41:22 IRST 2021
+ */
+
+package main.java.gui;
+
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author Alireza
+ */
+public class MainMenu extends JFrame {
+    public MainMenu() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
